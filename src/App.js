1e7a6f15d2d8808dffd336e196df533230de4ace
Sung M. Kim <sungmkim80@gmail.com>
2020-03-12 21:33:25 -0400

    saves tweets to the database

--- src/App.js
+++ src/App.js
@@ -1,9 +1,109 @@
 import React, { useEffect } from 'react'
+import styled from 'styled-components'
+import create from 'zustand'
+import { devtools } from 'zustand/middleware'
 
 import { useFirebase, useUser } from './firebase'
 
-import logo from './logo.svg'
 import './App.css'
+
+const [useStore] = create(
+  devtools(
+    set => ({
+      tweets: [],
+      addTweet: tweet =>
+        set(
+          state => ({
+            ...state,
+            tweets: [tweet, ...state.tweets]
+          }),
+          // https://github.com/react-spring/zustand/issues/77#issuecomment-551567711
+          'addTweet'
+        )
+    }),
+    'tweetStore'
+  )
+)
+
+const FormContainer = styled.section``
+const ListContainer = styled.section``
+const NameLabel = styled.label`
+  display: block;
+`
+const TextLabel = styled.label`
+  display: block;
+`
+
+function TweetForm() {
+  const addTweet = useStore(state => state.addTweet)
+  const { db } = useFirebase()
+  const user = useUser()
+
+  const submitTweet = e => {
+    e.preventDefault()
+    e.persist()
+    const tweet = { name: e.target['name'].value, text: e.target['text'].value }
+    console.info(`tweet=>`, tweet)
+
+    db.ref(`/tweets/${user.uid}`)
+      .push()
+      .set({
+        name: tweet.name,
+        text: tweet.text
+      })
+      .then(() => addTweet(tweet))
+
+    // addTweet(tweet)
+  }
+
+  return (
+    <form onSubmit={submitTweet}>
+      <NameLabel>
+        Name:
+        <input type='text' name='name' />
+      </NameLabel>
+      <TextLabel>
+        Name:
+        <textarea rows={3} cols={80} name='text' />
+      </TextLabel>
+      <button type='submit'>Tweet~</button>
+    </form>
+  )
+}
+
+function TweetList() {
+  const tweets = useStore(state => state.tweets)
+  const addTweet = useStore(state => state.addTweet)
+  const user = useUser()
+  const { db } = useFirebase()
+
+  useEffect(() => {
+    if (!user) return
+
+    db.ref(`/tweets/${user.uid}`)
+      .once('value')
+      .then(snapshot => {
+        const savedTweets = Object.values(snapshot.val())
+        console.info(`savedTweets ==>`, savedTweets)
+        savedTweets.map(addTweet)
+      })
+  }, [addTweet, db, user])
+
+  useEffect(() => {
+    console.info(`tweets ===>`, tweets)
+  }, [tweets])
+
+  return (
+    <ul>
+      {tweets.map((tweet, i) => (
+        <li key={i}>
+          <h4>{tweet.name}</h4>
+          <p>{tweet.text}</p>
+        </li>
+      ))}
+    </ul>
+  )
+}
 
 function App() {
   const { signIn, signOut, db } = useFirebase()
@@ -28,20 +128,13 @@ function App() {
     <div className='App'>
       <button onClick={signIn}>Sign IN</button>
       <button onClick={signOut}>Sign OUT</button>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+
+      <FormContainer>
+        <TweetForm />
+      </FormContainer>
+      <ListContainer>
+        <TweetList />
+      </ListContainer>
     </div>
   )
 }
